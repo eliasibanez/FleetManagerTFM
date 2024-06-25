1c1421ecd9610406ea4591eab3d121860239ac74
eliasibanez <elibcli@posgrado.upv.es>
2024-06-25 11:45:01 +0200

    Fixed a cast

--- src/main/java/com/elias/FleetManagerTFM/MainVerticle.java
+++ src/main/java/com/elias/FleetManagerTFM/MainVerticle.java
@@ -7,6 +7,7 @@ import com.elias.FleetManagerTFM.infrastructure.adapters.MqttClientAdapter;
 import com.elias.FleetManagerTFM.infrastructure.adapters.PgFleetDataRepositoryAdapter;
 import com.elias.FleetManagerTFM.infrastructure.configuration.VertxConfig;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.rxjava3.core.Vertx;
 import io.vertx.rxjava3.mqtt.MqttClient;
 import io.vertx.rxjava3.pgclient.PgPool;
 
@@ -18,7 +19,7 @@ public class MainVerticle extends AbstractVerticle {
     PgPool pgPool = (PgPool) configuration.configurePgPool(vertx);
     FleetDataRepositoryPort fleetDataRepository = new PgFleetDataRepositoryAdapter(pgPool);
     MqttClientAdapter mqttClientAdapter = configuration.configureMqttClient(vertx);
-    DataProcessingService dataProcessingService = new DataProcessingService(vertx);
+    DataProcessingService dataProcessingService = new DataProcessingService((Vertx) vertx);
     MqttSubscriberService mqttSubscriberService = new MqttSubscriberService((MqttClient) mqttClientAdapter.getClient(), dataProcessingService, fleetDataRepository);
 
     mqttClientAdapter.connect("localhost", 1883); // Para este TFM, se asume que el broker MQTT está en localhost
